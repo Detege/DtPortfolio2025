6ed8c9f28afcbdea777ea6aa8402f7f432760b89
Terol <terolgarcia@gmail.com>
2025-01-28 17:09:23 +0100

    format navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,34 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="p-4 bg-gray-800 text-white">
-      <ul className="flex space-x-4">
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/daniel-terol/">Contact</a>
-        </li>
-      </ul>
+    <nav className="fixed top-0 left-0 w-full bg-gray-800 text-white shadow-md">
+      <div className="mx-auto flex justify-between items-center py-4 px-8">
+        {/* Left Section - Name */}
+        <div className="text-xl font-bold">
+          <Link to="/home" className="hover:text-gray-400">
+            Daniel Terol
+          </Link>
+        </div>
+
+        {/* Right Section - Links */}
+        <ul className="flex space-x-6">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/daniel-terol/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-400"
+            >
+              Contact
+            </a>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-gray-400">
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
