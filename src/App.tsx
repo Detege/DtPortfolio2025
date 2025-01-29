ab7c261c7cbb9435900ec958f709db7788aecb2d
Terol <terolgarcia@gmail.com>
2025-01-29 16:21:00 +0100

    add darkmode functionality to the button

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,29 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { useState } from "react";
 
-const App = () => {
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
   return (
-    <Router>
-      <Navbar />
-
-      <div className="flex h-screen w-screen pt-16">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Intro />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className={`${darkMode ? "dark" : ""}`}>
+      <Router>
+        <Navbar />
+        <div className="flex h-screen w-screen pt-16">
+          <Sidebar darkMode={darkMode} setDarkMode={setDarkMode} />
+          <main className="flex-1 bg-gray-100 dark:bg-gray-900 p-8 flex items-center justify-center">
+            <div className="max-w-4xl text-center">
+              <Routes>
+                <Route path="/" element={<Intro />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+            </div>
+          </main>
+        </div>
+      </Router>
+    </div>
   );
 };
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+interface Props {
+  darkMode: boolean;
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const Sidebar: React.FC = () => {
-  const [darkMode, setDarkMode] = useState<boolean>(false);
-
+const Sidebar: React.FC<Props> = ({ darkMode, setDarkMode }) => {
   return (
     <aside className="w-64 bg-gray-200 dark:bg-gray-800 p-4 flex flex-col justify-between">
       <div className="space-y-4">
@@ -22,7 +23,7 @@ const Sidebar: React.FC = () => {
           onClick={() => setDarkMode(!darkMode)}
           className="w-full text-left p-2 rounded-md hover:bg-gray-300 dark:hover:bg-gray-700"
         >
-          Dark Mode
+          {darkMode ? "Light Mode" : "Dark Mode"}
         </button>
       </div>
     </aside>
